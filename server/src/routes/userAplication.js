fdd3dda848ad18b471c1ce1408c6796fb01c8da2
AllWKA <bryanjaramillobaldeon@gmail.com>
2019-02-25 23:54:59 +0000

    API DONE

--- server/src/routes/userAplication.js
+++ server/src/routes/userAplication.js
@@ -29,7 +29,7 @@ module.exports = app => {
             .then(app => { res.json(app);})
             .catch(error => { res.status(412).json({ msg: error.message }) });;
     });
-    app.post('/devicesFromUser', (req, res) => {
+    app.post('/userAplications', (req, res) => {
 
         // const aplicationName = req.body.aplicationName;
         // const tokenAplication = req.body.tokenAplication;
@@ -39,5 +39,39 @@ module.exports = app => {
             .catch(error => { res.status(412).json({ msg: error.message }) });
 
     });
+    app.put('/userAplications/:userID/:aplicationID/:deviceToken', (req, res) => {
 
+        const userID = req.body.userID;
+        const aplicationID = req.body.aplicationID;
+        
+        userApps.update(
+            req.body,
+            {
+                where: {
+                    userID: req.params.userID,
+                    aplicationID: req.params.aplicationID,
+                    deviceToken: req.params.deviceToken
+
+                }
+            }
+        )
+            .then(app => { res.json(app); })
+            .catch(error => { res.status(412).json({ msg: error.message }) });
+
+    });
+    app.delete('/userAplications/:userID/:aplicationID/:deviceToken', (req, res) => {
+
+        userApps.destroy(
+            {
+                where: {
+                    userID: req.params.userID,
+                    aplicationID: req.params.aplicationID,
+                    deviceToken: req.params.deviceToken
+                }
+            })
+
+            .then(app => { res.json(app); })
+
+            .catch(error => { res.status(412).json({ msg: error.message }); });
+    });
 };
