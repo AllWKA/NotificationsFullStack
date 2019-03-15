ba906cdead5cb6b8860557d68110edfa6c82e9fb
AllWKA <bryanjaramillobaldeon@gmail.com>
2019-03-15 17:48:14 +0000

    working on get applications from DB

--- ClienteWebAdmin/js/notificationsPanel.js
+++ ClienteWebAdmin/js/notificationsPanel.js
@@ -1,14 +1,57 @@
 window.onload = initialize;
 
-var notificationFrom;
 var notificationMessege;
 var notificationLabel;
 var notificationDate;
+var applicationSelected;
 
-function initialize(){
-    this.notificationFrom = document.getElementById("notificationForm");
-    this.notificationMessege = this.notificationFrom.namedItem("notificationMessege");
-    this.notificationLabel = this.notificationFrom.namedItem("notificationLabel");
-    this.notificationDate = this.notificationFrom.namedItem("deliveyDate");
-    
+var applications;
+
+function initialize() {
+    this.notificationMessege = document.getElementById("notificationMessege");
+    this.notificationLabel = document.getElementById("notificationLabel");
+    this.notificationDate = document.getElementById("deliveyDate");
+    this.applicationSelected = document.getElementById("applicationSelected");
+    getApplications();
+}
+
+
+
+function showApplications() {
+    console.log("applications pls");
+    if (applicationSelected.checked == true) {
+        console.log("si");
+    } else {
+        console.log("no");
+    }
+}
+
+function getApplications() {
+
+    console.log("getting applications");
+
+    var xhttp = new XMLHttpRequest(), method = "GET", url = "http://localhost:3000/apps";
+    xhttp.onreadystatechange = function () {
+        console.log("ready");
+
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            var applications = JSON.parse(xhttp.responseText);
+
+
+            if (applications != null) {
+
+
+                console.log(JSON.stringify(applications));
+
+
+            } else {
+                alert("Any app in DB");
+            }
+        } else {
+            alert("Something went wrong");
+        }
+    };
+    xhttp.open(method, url, true);
+    xhttp.send();
 }
